973116c4b4e8c64290c83878aace954c79297318
saipanindra <saipanindra9@gmail.com>
2015-05-16 14:57:56 -0700

    ONP

---
+++ ONP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                string expression = Console.ReadLine();
+                foreach (char c in expression.ToCharArray())
+                {
+                    if (c >= 'a' && c <= 'z')
+                        Console.Write(c);
+                    else if (c == ')' && stack.Count > 0)
+                    {
+                        char p = stack.Pop();
+                        Console.Write(p);
+                    }
+                    else if(c == '(') continue;
+                    else
+                        stack.Push(c);
+                }
+                Console.WriteLine();
+                stack.Clear();
+                t--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+                    
+                    
+
